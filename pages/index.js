bbbc9145a77b4ef32cd7e96823d4c24acc63ecba
Tal Forsher <talf@webiya.com>
2021-09-24 00:44:55 +0300

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react";
+import styles from "./styles.module.css";
+
+export default function App({ prices }) {
+  return (
+    <div className={styles.App}>
+      <h1>תחנות הדלק הזולות בישראל</h1> <h3>השוואת מחירים</h3>
+      <ul>
+        {prices.map((station) => (
+          <li key={station.title}>
+            {station.title} | {station.fuel_prices.customer_price.price}₪
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export async function getStaticProps(preview = true) {
+  const prices = await fetch(
+    "https://10ten.co.il/website_api/website/1.0/generalDeclaration"
+  )
+    .then((prices) => prices.json())
+    .then((prices) =>
+      prices.data.stationsArr.sort((a, b) => () =>
+        a.fuel_prices.customer_price.price <
+          b.fuel_prices.customer_price.price && -1
+      )
+    );
+  return {
+    props: {
+      prices
+    }
+  };
+}
