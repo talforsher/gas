08304bb1d2a37a14be642ba5d11c9753015a6ed6
Tal Forsher <talf@webiya.com>
2021-09-24 08:13:55 +0300

    Header

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+import Head from "next/head";
 import styles from "./styles.module.css";
 
 export default function App({ prices }) {
   return (
     <div className={styles.App}>
+      <Head>
+        <title>מלך הדלק | השוואת מחירי דלק</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <h1>תחנות הדלק הזולות בישראל</h1> <h3>השוואת מחירים</h3>
       <ul>
         {prices.map((station) => (
@@ -16,17 +21,17 @@ export default function App({ prices }) {
   );
 }
 
-export async function getStaticProps(preview = true) {
+export async function getStaticProps(preview = false) {
   const prices = await fetch(
     "https://10ten.co.il/website_api/website/1.0/generalDeclaration"
   )
-    .then((prices) => prices.json())
-    .then((prices) =>
-      prices.data.stationsArr.sort((a, b) =>
-        a.fuel_prices.customer_price.price <
-          b.fuel_prices.customer_price.price && -1
-      )
-    );
+    .then((res) => res.json())
+    .then((res) => res.data.stationsArr);
+  prices.sort(
+    (a, b) =>
+      a.fuel_prices.customer_price.price < b.fuel_prices.customer_price.price &&
+      -1
+  );
   return {
     props: {
       prices
